e051b273e483a3f1ebf67371536e78b8f9ef687a
Princy Ballabh <princyballabh13@gmail.com>
2025-05-17 20:28:03 +0530

    modifications made

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#328e6e", // Top green
+        secondary: "#67ae6e", // 2nd green
+        accent: "#90c67c", // 3rd green
+        pale: "#e1eebc", // Cream
+      },
+    },
+  },
+  plugins: [],
+};
